3b451c390318e9ded7f378d61029d7cbe4502c63
MrDeadCe11 <mister.deadce11@gmail.com>
2025-09-11 23:26:56 -0500

    created redemption during shutdown, need to reduce contract size

--- packages/contracts/contracts/Interfaces/ITroveManager.sol
+++ packages/contracts/contracts/Interfaces/ITroveManager.sol
@@ -159,17 +159,15 @@ interface ITroveManager is ILiquityBase {
 
     function shutdown(bool _oracleFailure) external;
 
-    function redeemCollateralDuringShutdown(
-        uint _LUSDamount,
-        address _firstRedemptionHint,
-        address _upperPartialRedemptionHint,
-        address _lowerPartialRedemptionHint,
-        address _upperShieldedPartialRedemptionHint,
-        address _lowerShieldedPartialRedemptionHint,
-        uint _partialRedemptionHintNICR,
-        uint _maxIterations,
-        uint _maxFeePercentage
-    ) external;
-
-    function calcRedemptionRateForShutdown(uint _LUSDAmount, uint _totalLUSDSupply) external view returns (uint);
+    // function redeemCollateralDuringShutdown(
+    //     uint _LUSDamount,
+    //     address _firstRedemptionHint,
+    //     address _upperPartialRedemptionHint,
+    //     address _lowerPartialRedemptionHint,
+    //     address _upperShieldedPartialRedemptionHint,
+    //     address _lowerShieldedPartialRedemptionHint,
+    //     uint _partialRedemptionHintNICR,
+    //     uint _maxIterations,
+    //     uint _maxFeePercentage
+    // ) external;
 }
