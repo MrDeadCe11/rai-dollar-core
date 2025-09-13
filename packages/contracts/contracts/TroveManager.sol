a0becf787bee8af7ddeab094e792651d7f1d0a30
MrDeadCe11 <mister.deadce11@gmail.com>
2025-09-13 00:25:20 -0500

    fixed partial redemption cancel bug

--- packages/contracts/contracts/TroveManager.sol
+++ packages/contracts/contracts/TroveManager.sol
@@ -295,6 +295,10 @@ contract TroveManager is LiquityBase, Ownable, CheckContract, ITroveManager, ITr
         } else {
 
             singleRedemption = _reInsertTroves(_contractsCache, locals, hints, singleRedemption, _shielded, _borrower);
+            // if the partial redemption is cancelled during  the reinsertion, return the singleRedemption
+            if(singleRedemption.cancelledPartial) {
+                return singleRedemption;
+            }
 
             Troves[_borrower].debt = locals.newDebt;
             Troves[_borrower].coll = locals.newColl;
