b533ef2a9798b9297a718e0875cca62c1af8c01c
MrDeadCe11 <mister.deadce11@gmail.com>
2025-09-20 12:26:01 -0500

    working on partial redemptions at 100% discount

--- packages/contracts/contracts/TroveManager.sol
+++ packages/contracts/contracts/TroveManager.sol
@@ -267,7 +267,20 @@ contract TroveManager is LiquityBase, Ownable, CheckContract, ITroveManager, ITr
         if(_redemptionLocals.shutdown) {
             singleRedemption.LUSDLot = LiquityMath._min(_maxLUSDAmount, _actualDebt(t.debt, _shielded));
             // do not take fee during shutdown
-            singleRedemption.collateralLot = singleRedemption.LUSDLot.mul(_redemptionLocals.par).mul(DECIMAL_PRECISION).div(DECIMAL_PRECISION.sub(_calcDiscount()).mul(_redemptionLocals.price));
+            uint256 discount = _calcDiscount();
+            if(discount == DECIMAL_PRECISION) {
+                // denominator is zero → only allow full redemption to take all collateral
+                if (singleRedemption.LUSDLot == _actualDebt(t.debt, _shielded)) {
+                    singleRedemption.collateralLot = t.coll;
+                } else {
+                    // partial redemption impossible at 100% discount; skip this trove
+                     singleRedemption.cancelledPartial = true; // stop here, leave remaining LUSD unused
+                    return singleRedemption;
+                }
+            } else {
+                singleRedemption.collateralLot = singleRedemption.LUSDLot.mul(_redemptionLocals.par).mul(DECIMAL_PRECISION).div(DECIMAL_PRECISION.sub(discount).mul(_redemptionLocals.price));
+            }
+                     
         } else {
             singleRedemption.LUSDLot = LiquityMath._min(_maxLUSDAmount, _actualDebt(t.debt, _shielded).sub(LUSD_GAS_COMPENSATION));
             singleRedemption.collateralLot = singleRedemption.LUSDLot.mul(_redemptionLocals.par).div(_redemptionLocals.price);
